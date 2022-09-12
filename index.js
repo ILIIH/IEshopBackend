c4be655317b8866343444d84f450c70faa1306c9
ILIIH <jointlord0@gmail.com>
2022-09-12 16:54:21 +0100

    added JSON parser

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 const mysql = require('mysql')
 
 const port = process.env.PORT || 3000
