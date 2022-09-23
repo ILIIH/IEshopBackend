7647583003e81e336225e9cd57d314babf95822f
ILIIH <jointlord0@gmail.com>
2022-09-23 22:11:53 +0100

    mail changet signature

--- index.js
+++ index.js
@@ -56,10 +56,10 @@ app.get('/api/sendEmail/:email/:code', (req, res) => {
             transporter.sendMail(mailOptions, function(error, info){
               if (error) {
                 console.log(error);
-                res.json(`{"Error" : "true" , "measage" : "${error.message}"}`)
+                res.json(`{Error : true , measage : ${error.message}}`)
               } else {
                 console.log('Email sent: ' + info.response);
-                res.json(`{"Error" : "false" , "measage" : "${info.response}"}`)
+                res.json(`{Error : false , measage : ${info.response}}`)
               }
             });           
 
